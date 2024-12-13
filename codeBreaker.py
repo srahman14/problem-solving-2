5b4578b061dd4b47d630c364aff5f7e06f2e6c33
srahman14 <s.rahman14@hotmail.com>
2024-12-13 23:00:29 +0000

    comments added, fully functional

--- codeBreaker.py
+++ codeBreaker.py
@@ -1,7 +1,7 @@
-# Constraints: n >= 10,000
+# Constraints: n <= 10,000
 
 # n = int(input("Enter the number n 2-10,000:  "))
-n = 30
+n = 3999
 
 # # base case:
 # if n == 2 or n == 3:
@@ -24,23 +24,60 @@ while i < len(list_int):
 # Prime factorisation
 
 def primeFactorisation(n):
+    # Necessary to use a dictionary so I can attach the number of times
+    # a prime factor is counted and then produce it after in a format
+    # desired
     prime_factors = {}
     for i in list_int:
+        # if the current number in list cannot divide into the current 
+        # value of N, skip to the next value  
         if n % i != 0:
             continue
+        # Hold the number of times that the current prime goes into N, 
         count = 0
+        # The loop will automatically break out after the prime no longer
+        # divides into N
         while n % i == 0:
             n = n // i
             count += 1 
+        # Attach the number of times the prime goes into N in the dictionary
         prime_factors[i] = count
+        # I.e. N can no longer be divided down anymore
         if n == 1:
             break
+    
     results = []
+    print(prime_factors)
     for i in prime_factors:
-        if prime_factors[i] > 1:
+        # if the prime factor has more than one digit, it has to be handled using
+        # .append() so that it's whole value is appended, i.e. if a prime factor was
+        # 43, using .append() => '43', where as using .extend() => '4', '3'
+        # .extend() is useful where needed to repeat a prime factor, i.e. if a prime
+        # factor such as 2 was repeated 5 times, .append() => '22222', where as using
+        # .extend() => '2', '2', '2', '2', '2'
+
+        # Checking if a prime factor has more than one digit
+        if len(str(i)) > 1:
+            # Checking the number of times the prime factor is repeated and adding it 
+            # to the list that many times
+            if prime_factors[i] > 1:
+                results.append(str(i) * prime_factors[i])
+            else:
+                # If only repeated once only added once
+                results.append(str(i))
+
+        # Checking for prime factors of only one digit
+        elif prime_factors[i] > 1:
+            # Add the prime factors for the number of times that it divided into N
+            # to the results list
             results.extend((str(i)) * prime_factors[i])
         else:
+            # If it only divided into N once, it is only added once
             results.extend(str(i))
-
+    # Final results separated by 'x' using .join()
+    print(results)
     return " x ".join(results)
-print(primeFactorisation(10000))
+
+result = primeFactorisation(n)
+print(result)
+
